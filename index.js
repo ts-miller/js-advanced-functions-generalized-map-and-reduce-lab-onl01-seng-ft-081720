701163f3463a7f16901f8286b18480ef16bc68ea
ts-miller <tylerstephen11@gmail.com>
2020-11-30 22:39:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, callback, start) {
+    let r = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        r = callback(array[i], r)
+    }
+    return r
+}
